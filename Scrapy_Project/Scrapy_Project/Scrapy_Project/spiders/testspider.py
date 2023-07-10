610fe21b3c86c870a713e52c1e7b2a2fcd49bf98
Hifza Majeed <p191652@nu.edu.pk>
2023-07-10 13:24:53 +0500

    Using Yield vaiable

--- Scrapy_Project/Scrapy_Project/Scrapy_Project/spiders/testspider.py
+++ Scrapy_Project/Scrapy_Project/Scrapy_Project/spiders/testspider.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import scrapy
 class QuotesSpider(scrapy.Spider):
+    """""
+    #without yield
     name = "quotes"
     start_urls = [
         "https://quotes.toscrape.com/page/1/",
@@ -12,3 +14,18 @@ class QuotesSpider(scrapy.Spider):
         filename = f"quotes-{page}.html"
         Path(filename).write_bytes(response.body)
         self.log(f"Saved file {filename}")
+        """
+    # using yield
+    name = "quotes"
+    start_urls = [
+        "https://quotes.toscrape.com/page/1/",
+        "https://quotes.toscrape.com/page/2/",
+    ]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
